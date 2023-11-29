abd3120cd3b9d2bccae2aef42c30494cac857900
Seef Almostafa <s19ca5@umwelt-campus.de>
2023-11-29 11:29:29 +0100

    init app

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -15,19 +14,28 @@ class Todo(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
 
+# Constant error messages
+ERROR_ADD_TASK = 'There was an issue adding your task'
+ERROR_DELETE_TASK = 'There was a problem deleting that task'
+ERROR_UPDATE_TASK = 'There was an issue updating your task'
+ERROR_EMPTY_CONTENT = 'Task content cannot be empty'
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        task_content = request.form['content']
-        new_task = Todo(content=task_content)
+        task_content = request.form['content'].strip()
 
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue adding your task'
+        if task_content:
+            new_task = Todo(content=task_content)
+
+            try:
+                db.session.add(new_task)
+                db.session.commit()
+                return redirect('/')
+            except:
+                return ERROR_ADD_TASK
+        else:
+            return ERROR_EMPTY_CONTENT
 
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
@@ -43,24 +51,29 @@ def delete(id):
         db.session.commit()
         return redirect('/')
     except:
-        return 'There was a problem deleting that task'
+        return ERROR_DELETE_TASK
 
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
-    task = Todo.query.get_or_404(id)
+    task_to_update = Todo.query.get_or_404(id)
 
     if request.method == 'POST':
-        task.content = request.form['content']
+        new_content = request.form['content'].strip()
 
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue updating your task'
+        # Check if the updated content is not empty before updating
+        if new_content:
+            task_to_update.content = new_content
+
+            try:
+                db.session.commit()
+                return redirect('/')
+            except:
+                return ERROR_UPDATE_TASK
+        else:
+            return ERROR_EMPTY_CONTENT
 
     else:
-        return render_template('update.html', task=task)
-
+        return render_template('update.html', task=task_to_update)
 
 if __name__ == "__main__":
     app.run(debug=True)
